205b7f3f0a8334c9b67b0ad993363ddbadf7ad66
austinsaugrich <asaugrich19@gmail.com>
2024-05-07 02:45:11 -0500

    added pngs

--- client/src/components/ChampInfo.jsx
+++ client/src/components/ChampInfo.jsx
@@ -1,5 +1,6 @@
 import placeholder from "../assets/mid.png";
 
+import { useEffect, useState } from "react";
 export default function ChampInfo({
   champname,
   lanename,
@@ -9,20 +10,45 @@ export default function ChampInfo({
   rerollfunc,
   lanepref,
 }) {
-  console.log(lanepref);
+  const [imgurl, SetImgUrl] = useState("");
+  const [lanes, Setlanes] = useState("");
+  useEffect(() => {
+    if (champname !== "Wukong") {
+      SetImgUrl(
+        `https://cdn.mobalytics.gg/assets/lol/images/dd/champions/icons/${champname
+          .toLowerCase()
+          .replace(/ /g, "")
+          .replace(/'/g, "")
+          .replace(/\./g, "")}.png?V3`
+      );
+    } else {
+      SetImgUrl(
+        `https://cdn.mobalytics.gg/assets/lol/images/dd/champions/icons/monkeyking.png?V3`
+      );
+    }
+
+    let temp = "";
+    for (let i = 0; i < lanename.length; i++) {
+      if (i == 0) {
+        temp = lanename[0];
+      } else {
+        temp += `/${lanename[i]}`;
+      }
+    }
+    Setlanes(temp);
+  }, [lanename, champname]);
   return (
     <div className='chosencontainer'>
       <div className='imgdiv'>
-        <img src={placeholder} alt='' />
+        <img src={imgurl} aria-label='champimg' role='img' />
       </div>
       <div className='middlediv'>
+        <p>The chosen champion is {champname}</p>
+        <p>They are played in the {lanes} lane.</p>
         <p>
-          The champion chosen is {champname}. They are played in the{" "}
-          {lanename + " "}
-          lane. Their attack type is {attacktype} and they are {attack}. You can
-          find their build here.
+          Their attack type is {attacktype} and they are {attack}
         </p>
-        <button onClick={() => rerollfunc(lanepref)}>Re-Roll</button>
+        <button onClick={() => rerollfunc(lanepref)}>Re-roll</button>
       </div>
       <div className='rightdiv'>
         <button onClick={() => closefunc()}>X</button>
